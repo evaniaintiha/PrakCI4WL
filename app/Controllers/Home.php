9129024d6cc8661c5a1762f7cb10063c5a4f7996
evaniaintiha <Evania.intiha@yahoo.com>
2020-10-16 21:45:52 +0700

    Praktikum Tugas 2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,15 @@ class Home extends BaseController
 	{
 		return view('welcome_message');
 	}
-
+	public function show()
+	{
+		$data['title'] = 'Mahasiswa';
+		$data['nama'] = 'Evania Intiha';
+		$data['npm'] = '1817051044';
+		echo view('mahasiswa/header', $data);
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer');
+	}
 	//--------------------------------------------------------------------
 
 }
